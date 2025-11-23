c8f4c7564fcd2d0e88ad963fc02fb639591bcdd3
FernandoGutemberg <fernando70sil@gmail.com>
2025-11-23 17:08:10 -0300

    execução-teste-automatico_V2

--- automacao-cypress/cypress.config.js
+++ automacao-cypress/cypress.config.js
@@ -4,7 +4,6 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: "https://analista-teste.seatecnologia.com.br/",
     setupNodeEvents(on, config) {
-      // implement node event listeners here
     },
   },
 });
